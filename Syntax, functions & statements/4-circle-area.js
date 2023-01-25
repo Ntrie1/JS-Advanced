1939a26e511396d560548bde2685932f3cf7e9ec
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2023-01-25 19:29:12 +0200

    Add files via upload

---
+++ Syntax, functions & statements/4-circle-area.js
@@ -0,0 +1,16 @@
+function circleArea(input){
+
+    let inputType = typeof input;
+    let result;
+
+    if (inputType === 'number') {
+        result = (Math.pow(input, 2) * Math.PI).toFixed(2);
+        console.log(result);
+    } else {
+        console.log(`We can not calculate the circle area, because we receive a ${inputType}.`);
+    }
+
+    
+
+
+}circleArea(5);
