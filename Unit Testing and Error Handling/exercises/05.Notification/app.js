9810e6f016d5b94d44c23b51406be446582bd0b4
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2023-03-30 15:44:35 +0300

    Add files via upload

---
+++ Unit Testing and Error Handling/exercises/05.Notification/app.js
@@ -0,0 +1,11 @@
+function notify(message) {
+ 
+  let notification = document.getElementById('notification');
+  notification.innerText = message;
+  notification.style.display = 'block';
+
+  notification.addEventListener('click',(event)=>{
+    event.target.style.display = 'none';
+  })
+
+}
